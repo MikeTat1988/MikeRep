027dd5101a7fba191115f60f4b95a20d235eb87a
MikeTat1988 <michaeltatev@gmail.com>
2023-06-12 12:08:48 +0300

    PubSub

---
+++ AdvancedC#/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class NotificationService
+{
+    public delegate void NotificationDelegate (string prameter);
+    public event NotificationDelegate? NotificationEvent;
+
+    public void NotifySubscribers(string message)
+    {
+        NotificationEvent?.Invoke(message);
+    }
+
+    public void Sub(Subscriber subscriber)
+    {
+        NotificationEvent += subscriber.HandleNotification;
+    }
+
+    public void Unsub(Subscriber subscriber)
+    {
+        NotificationEvent -= subscriber.HandleNotification;
+    }
+}
+
+public class Subscriber
+{
+    public NotificationService mynote { get; set; }
+    public Subscriber(NotificationService notification)
+    {
+        mynote = notification;
+    }
+
+    public void HandleNotification(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        NotificationService notificationService = new NotificationService();
+
+        Subscriber subscriber1 = new Subscriber(notificationService);
+        Subscriber subscriber2 = new Subscriber(notificationService);
+        Subscriber subscriber3 = new Subscriber(notificationService);
+
+        notificationService.Sub(subscriber1);
+        notificationService.Sub(subscriber2);
+        notificationService.Sub(subscriber3);
+
+
+        notificationService.NotifySubscribers("hello");
+
+        Console.ReadLine();
+    }
+
+}
+
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,18 +12,18 @@ namespace hrd27
     {
         static void Main(string[] args)
         {
-            //ThisStack<int> mystack = new ThisStack<int>();
+            ThisStack<int> mystack = new ThisStack<int>();
 
-            //LList<int> myList = new LList<int>();
+            LList<int> myList = new LList<int>();
 
-            //myList.Push(10);
-            //myList.Push(20);
-            //myList.Push(30);
+            myList.Push(10);
+            myList.Push(20);
+            myList.Push(30);
 
-            //foreach (int item in myList)
-            //{
-            //    Console.WriteLine(item);
-            //}
+            foreach (int item in myList)
+            {
+                Console.WriteLine(item);
+            }
 
             Shape circ = new Circle(2, "white", true);
 
