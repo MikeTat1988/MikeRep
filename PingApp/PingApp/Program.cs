f27c46e2e48f32fd55afab198552ca49428a4e02
MikeTat1988 <michaeltatev@gmail.com>
2023-06-18 11:55:12 +0300

    pingpongnamed

---
+++ PingApp/PingApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Text;
+
+class PingProgram
+{
+    static void Main()
+    {
+        Console.WriteLine("Ping application started. Press any key to send a Ping. Press 'Q' to quit.");
+
+        while (true)
+        {
+            using (var pipeServer = new NamedPipeServerStream("PingPongPipe"))
+            {
+                pipeServer.WaitForConnection();
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.Q)
+                    break;
+
+                using (var writer = new StreamWriter(pipeServer, Encoding.UTF8, 4096, leaveOpen: true))
+                {
+                    writer.WriteLine("Ping");
+                }
+
+                Console.WriteLine("Pong");
+            }
+        }
+    }
+}
---
+++ PongApp/PongApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Text;
+
+class PongProgram
+{
+    static void Main()
+    {
+        Console.WriteLine("Pong application started. Waiting for Ping...");
+
+        while (true)
+        {
+            using (var pipeClient = new NamedPipeClientStream(".", "PingPongPipe"))
+            {
+                pipeClient.Connect();
+
+                using (var reader = new StreamReader(pipeClient, Encoding.UTF8))
+                {
+                    string message = reader.ReadLine();
+                    Console.WriteLine($"Received: {message}");
+                }
+            }
+        }
+    }
+}
